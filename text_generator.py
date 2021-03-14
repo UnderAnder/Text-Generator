022c981960148cd3af2f0ba4bb41092268d4cad1
UnderAnder <andryushin.alexander@gmail.com>
2021-03-14 20:00:36 +0700

    2/6 bigrams

--- text_generator.py
+++ text_generator.py
@@ -1,26 +1,30 @@
+from nltk.util import bigrams
 from nltk.tokenize import WhitespaceTokenizer
 
-def main():
-    tokens = []
-    with open(input(), "r", encoding="utf-8") as f:
-        for line in f:
-            tokens.extend(WhitespaceTokenizer().tokenize(line))
+
+def corpus_statistic(tokens):
     print('Corpus statistics')
     print('All tokens:', len(tokens))
     print('Unique tokens:', len(set(tokens)))
 
+
+def main():
+    with open(input(), "r", encoding="utf-8") as f:
+        tokens = WhitespaceTokenizer().tokenize(f.read())
+
+    bigrams_list = list(bigrams(tokens))
+    print('Number of bigrams:', len(bigrams_list))
+
     while True:
-        command = input()
+        command = input().strip().lower()
         if command == 'exit':
             exit()
         try:
-            print(tokens[int(command)])
-        except TypeError:
-            print('Type Error. Please input an integer.')
+            print(f'Head: {bigrams_list[int(command)][0]} \t Tail: {bigrams_list[int(command)][1]}')
+        except (TypeError, ValueError):
+            print('Typ Error. Please input an integer.')
         except IndexError:
             print('Index Error. Please input an integer that is in the range of the corpus.')
-        except ValueError:
-            print('Type Error.')
         else:
             continue
 
