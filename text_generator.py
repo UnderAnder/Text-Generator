5503bbe367bffb789c538dca09d1966a4d988435
UnderAnder <andryushin.alexander@gmail.com>
2021-03-15 22:50:56 +0700

    6/6 Generate sentences based on trigrams

--- text_generator.py
+++ text_generator.py
@@ -3,16 +3,40 @@ from random import choices
 from re import fullmatch
 
 from nltk.tokenize import WhitespaceTokenizer
-from nltk.util import bigrams
+from nltk.util import trigrams
 
 
 def markov_chain(tokens):
     model = defaultdict(Counter)
-    for head, tail in bigrams(tokens):
-        model[head][tail] += 1
-
+    for head1, head2, tail in trigrams(tokens):
+        model[f'{head1} {head2}'][tail] += 1
     return model
 
+
+def make_sequence(model):
+    first_word_pattern = r"[A-Z][\w\d']*"
+    end_word_pattern = r"[\S]*[.!?]"
+
+    list_keys = [*model]
+    # A random capitalized word that does not end with a sentence-ending punctuation mark
+    sequence = choices(list_keys)
+    while not fullmatch(first_word_pattern, sequence[0].split()[0]):
+        sequence = choices(list_keys)
+    # Minimal sentence length 5 tokens, with a sentence-ending punctuation mark
+    second = choices([*model[sequence[0]]])
+    sequence.extend(second)
+
+    key = f'{sequence[0].split()[1]} {second[0]}'
+
+    for i in range(1, 100):  # not less then 5 tokens, till sentence-ending punctuation mark
+        weights = [w for w in model[key].values()]
+        next_word = choices(list(model[key]), weights)
+        key = f'{sequence[i]} {next_word[0]}'
+        sequence.extend(next_word)
+
+        if i > 1 and fullmatch(end_word_pattern, next_word[0]):
+            print(*sequence)
+            break
 
 def corpus_statistic(tokens):
     print('Corpus statistics')
@@ -25,23 +49,9 @@ def main():
         tokens = WhitespaceTokenizer().tokenize(f.read())
 
     model = markov_chain(tokens)
-    first_word_pattern = r"[A-Z][\w\d']*"
-    end_word_pattern = r"[\S]*[.!?]"
 
-    # make 10 sequence
     for _ in range(10):
-        # first capitalized word and don't ends with a sentence-ending punctuation mark
-        seq = choices(list(model.keys()))
-        while not fullmatch(first_word_pattern, seq[0]):
-            seq = choices(list(model.keys()))
-        # minimal sentence length 5 tokens, with a sentence-ending punctuation mark
-        for i in range(100):
-            weights = [w for w in model[seq[i]].values()]
-            next_word = choices(list(model[seq[i]]), weights)
-            seq.extend(next_word)
-            if i > 2 and fullmatch(end_word_pattern, next_word[0]):
-                break
-        print(*seq)
+        make_sequence(model)
 
 
 if __name__ == '__main__':
